1afad2fe920696445fafdb2d457b52b57bd8ae81
Andy Clark <ajc97@cantab.net>
2016-09-12 19:28:22 +0100

    Initial commit

---
+++ src/ThemeGetter.cpp
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : ThemeGetter.cpp
+ Author      : Andy Clark
+ Version     :
+ Copyright   : Copyright (C) 2016 Andy Clark
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	cout << "Hello World" << endl; /* prints Hello World */
+	return 0;
+}
